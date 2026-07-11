a3b4e2282db729fa0fe1773fb095933b94d520d3
3619282776 <3619282776@qq.com>
2026-07-11 16:22:13 +0800

    zzz

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -285,7 +285,7 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 7199;
+  htim1.Init.Prescaler = 
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = 9;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
